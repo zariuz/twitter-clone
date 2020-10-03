8313a20a3f4ca5da407adce209d3f57714361d20
zariuz <nechetov@hotmail.com>
2020-10-03 12:41:16 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
